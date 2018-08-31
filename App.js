7e0ba30f84e21f0bf0a0174515c5097f4dc52fc5
Mark Boyd <markdavidboyd@gmail.com>
2018-08-31 12:10:21 -0400

    add sound recorder package to support recording audio & implement simple record button in app

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+import SoundRecorder from 'react-native-sound-recorder';
 
 export default class App extends React.Component {
+  record() {    
+    SoundRecorder.start(SoundRecorder.PATH_CACHE + '/test.mp4')
+      .then(function() {
+        console.log('started recording');
+        setTimeout(() => {
+          SoundRecorder.stop()
+            .then(function(result) {
+              console.log('stopped recording, audio file saved at: ' + result.path);
+            });          
+        }, 2000);
+      });      
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <Button 
+          onPress={() => this.record()}
+          title="Record"
+        >
+        </Button>
       </View>
     );
   }
