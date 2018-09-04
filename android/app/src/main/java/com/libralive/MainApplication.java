1b16e31ec007d421ba8e1830a80c162ae5b49c0f
Mark Boyd <markdavidboyd@gmail.com>
2018-09-04 14:40:56 -0400

    add native code links for react-native-audio package

--- android/app/src/main/java/com/libralive/MainApplication.java
+++ android/app/src/main/java/com/libralive/MainApplication.java
@@ -3,6 +3,7 @@ package com.libralive;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnim.rn.audio.ReactNativeAudioPackage;
 import com.kevinresol.react_native_sound_recorder.RNSoundRecorderPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeAudioPackage(),
             new RNSoundRecorderPackage()
       );
     }
