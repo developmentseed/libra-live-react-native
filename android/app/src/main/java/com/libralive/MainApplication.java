4c663806e236a921e923b78aebf8814b6420dc93
Mark Boyd <markdavidboyd@gmail.com>
2018-09-04 15:32:17 -0400

    add and link react-native-svg package

--- android/app/src/main/java/com/libralive/MainApplication.java
+++ android/app/src/main/java/com/libralive/MainApplication.java
@@ -3,6 +3,7 @@ package com.libralive;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.horcrux.svg.SvgPackage;
 import com.rnim.rn.audio.ReactNativeAudioPackage;
 import com.kevinresol.react_native_sound_recorder.RNSoundRecorderPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SvgPackage(),
             new ReactNativeAudioPackage(),
             new RNSoundRecorderPackage()
       );
