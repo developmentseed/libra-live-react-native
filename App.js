4209eb8b0118cb4d528a66e03739af86454ccaf0
Mark Boyd <markdavidboyd@gmail.com>
2018-09-05 18:13:44 -0400

    update app to use new LibraLexBot

--- App.js
+++ App.js
@@ -111,7 +111,7 @@ export default class App extends React.Component {
 
     const params = {
       botAlias: '$LATEST',
-      botName: 'LibraLive',
+      botName: 'LibraLexBot',
       contentType: 'audio/lpcm; sample-rate=8000; sample-size-bits=16; channel-count=1; is-big-endian=false',
       inputStream: audioBuffer,
       userId: `LibraLexBot${Date.now()}`,
