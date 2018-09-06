962a9f4e6a272a6af2411ad6603b351b00f7fc2c
Mark Boyd <markdavidboyd@gmail.com>
2018-09-06 14:56:30 -0400

    send city slot value from Lex response to Mapbox for geocoding and display map

--- App.js
+++ App.js
@@ -8,8 +8,11 @@ import {
 
 import { AudioRecorder, AudioUtils } from 'react-native-audio';
 import Config from 'react-native-config';
+import MapboxGL from '@mapbox/react-native-mapbox-gl';
 
 import AWS from 'aws-sdk/dist/aws-sdk-react-native';
+
+MapboxGL.setAccessToken(Config.MAPBOX_ACCESS_TOKEN);
 
 // Initialize the Amazon Cognito credentials provider
 AWS.config.region = 'us-east-1';
@@ -20,6 +23,12 @@ AWS.config.credentials = new AWS.CognitoIdentityCredentials({
 // import MicrophoneIcon from './app/components/MicrophoneIcon';
 
 const { Buffer } = require('buffer');
+
+const mapboxClient = require('@mapbox/mapbox-sdk');
+const mapboxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
+
+const baseClient = mapboxClient({ accessToken: Config.MAPBOX_ACCESS_TOKEN });
+const geocodingService = mapboxGeocoding(baseClient);
 
 const colorWhite = '#fff';
 const styles = StyleSheet.create({
@@ -36,6 +45,7 @@ export default class App extends React.Component {
     super(props);
 
     this.state = {
+      centerCoords: null,
       isAuthorized: false,
       isRecording: false,
     };
@@ -96,7 +106,7 @@ export default class App extends React.Component {
     }
   }
 
-  sendToLex(audioData) {
+  async sendToLex(audioData) {
     const { isRecording } = this.state;
     if (isRecording) {
       return;
@@ -118,13 +128,27 @@ export default class App extends React.Component {
       accept: 'text/plain; charset=utf-8',
     };
 
-    lexRuntime.postContent(params, (err, data) => {
-      if (err) {
-        console.log(err, err.stack);
-        return;
-      }
+    const lexResponse = await lexRuntime.postContent(params).promise();
+    console.log(lexResponse);
+    if (!lexResponse || !lexResponse.slots.City) {
+      return;
+    }
 
-      console.log(data);
+    const geoResponse = await geocodingService.forwardGeocode({
+      query: lexResponse.slots.City,
+      limit: 1,
+      countries: ['US'],
+    }).send();
+
+    if (!geoResponse.body.features || !geoResponse.body.features.length) {
+      return;
+    }
+
+    console.log(geoResponse);
+
+    const feature = geoResponse.body.features[0];
+    this.setState({
+      centerCoords: feature.geometry.coordinates,
     });
   }
 
@@ -136,7 +160,7 @@ export default class App extends React.Component {
   }
 
   render() {
-    const { isRecording } = this.state;
+    const { isRecording, centerCoords } = this.state;
 
     return (
       <View style={styles.container}>
@@ -150,6 +174,14 @@ export default class App extends React.Component {
           }}
           title={`${isRecording ? 'Stop' : 'Start'} recording`}
         />
+        { centerCoords && (
+          <MapboxGL.MapView
+            styleURL={MapboxGL.StyleURL.Street}
+            zoomLevel={12}
+            centerCoordinate={centerCoords}
+            style={{ flex: 1, width: '100%' }}
+          />
+        ) }
         {/* <MicrophoneIcon width={100} height={100} /> */}
       </View>
     );
