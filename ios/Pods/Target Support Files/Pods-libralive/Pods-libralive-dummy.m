60f791de338061fc82030d8e516d357ab310ecb7
Mark Boyd <markdavidboyd@gmail.com>
2018-09-07 16:56:33 -0400

    commit pod dependencies for app

---
+++ ios/Pods/Target Support Files/Pods-libralive/Pods-libralive-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_libralive : NSObject
+@end
+@implementation PodsDummy_Pods_libralive
+@end
---
+++ ios/Pods/Target Support Files/Pods-libraliveTests/Pods-libraliveTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_libraliveTests : NSObject
+@end
+@implementation PodsDummy_Pods_libraliveTests
+@end
