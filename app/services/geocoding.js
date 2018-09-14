91158ffb9b3ec65dac85a0f481bdbff68a42fff5
Mark Boyd <markdavidboyd@gmail.com>
2018-09-14 15:22:18 -0400

    removfe console.logs

--- app/services/geocoding.js
+++ app/services/geocoding.js
@@ -3,8 +3,6 @@ import Config from 'react-native-config';
 const mapboxClient = require('@mapbox/mapbox-sdk');
 const mapboxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
 
-console.log('Config', Config)
-
 const baseClient = mapboxClient({ accessToken: Config.MAPBOX_ACCESS_TOKEN });
 const geocodingService = mapboxGeocoding(baseClient);
 
