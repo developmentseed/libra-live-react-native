b7c1a469b3818fb452ef39495c226c92e7f740c8
Mark Boyd <markdavidboyd@gmail.com>
2018-09-14 18:00:42 -0400

    update Android build files for appcenter integration

--- android/app/src/main/java/com/libralive/MainApplication.java
+++ android/app/src/main/java/com/libralive/MainApplication.java
@@ -3,6 +3,7 @@ package com.libralive;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.microsoft.appcenter.reactnative.crashes.AppCenterReactNativeCrashesPackage;
 import com.microsoft.appcenter.reactnative.appcenter.AppCenterReactNativePackage;
 import com.microsoft.appcenter.reactnative.analytics.AppCenterReactNativeAnalyticsPackage;
 import com.mapbox.rctmgl.RCTMGLPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new AppCenterReactNativeCrashesPackage(MainApplication.this, getResources().getString(R.string.appCenterCrashes_whenToSendCrashes)),
             new AppCenterReactNativePackage(MainApplication.this),
             new AppCenterReactNativeAnalyticsPackage(MainApplication.this, getResources().getString(R.string.appCenterAnalytics_whenToEnableAnalytics)),
             new RCTMGLPackage(),
