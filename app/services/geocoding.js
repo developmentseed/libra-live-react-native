4c7432cf7d262975e4f7d931b30dd88b78ac5e66
Mark Boyd <markdavidboyd@gmail.com>
2018-09-17 18:12:30 -0400

    remove restriction to only geocode US cities & accept city/state for US cities

--- app/services/geocoding.js
+++ app/services/geocoding.js
@@ -6,10 +6,9 @@ const mapboxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
 const baseClient = mapboxClient({ accessToken: Config.MAPBOX_ACCESS_TOKEN });
 const geocodingService = mapboxGeocoding(baseClient);
 
-export const geocodeCityInput = async city => geocodingService.forwardGeocode({
-  query: city,
+export const geocodeCityInput = async ({ City: city, State: state = '' }) => geocodingService.forwardGeocode({
+  query: `${city} ${state}`,
   limit: 1,
-  countries: ['US'],
 }).send();
 
 export default {
